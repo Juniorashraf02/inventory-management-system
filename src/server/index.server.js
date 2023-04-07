8418b5cab89647fecc2be265182d4a01695051c2
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-04-07 14:52:18 +0600

    project setup done

---
+++ src/server/index.server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+// server
+const port = process.env.PORT || 7000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res
+    .status(201)
+    .send("backend for inventory management system is running...!");
+});
+
+app.all("*", (req, res) => {
+  return res.send("No route found");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`.bgBlue);
+});
