e84e5f9d54f19d0b9a1588074a87249578afc12d
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-06-05 22:03:17 +0600

    getproducts file changed

--- src/controller/ProductController/getProduct.js
+++ src/controller/ProductController/getProduct.js
@@ -1,7 +1,14 @@
 const Product = require("../../models/product.schema");
 
 exports.getProducts = async (req, res, next) => {
-  const data = await Product.find({});
+  console.log(req.query);
+  const queryObject = {...req.query};
+  const excludedFields = ['sort', 'page', 'limit', 'quantity'];
+
+  excludedFields.forEach(fields=> delete queryObject[fields]);
+
+  const data = await Product.find(queryObject);
+  console.log(queryObject);
 
   res.status(201).json({
     data: data,
